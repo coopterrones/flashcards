3087797ebce984e6453f61ea3805d4615efb12f0
Cooper Terrones <cooper.terrones@outlook.com>
2020-10-07 22:52:07 -0600

    Add feature/game

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,30 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Turn = require('./Turn');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+    this.deck;
+  }
+
+  start() {
+    let cards = [];
+    prototypeQuestions.forEach(card => {
+
+      let newCard = new Card(card['id'], card['question'], card['answers'], card['correctAnswer']);
+      cards.push(newCard);
+      this.deck = new Deck(cards);
+    });
+
+    this.currentRound = new Round(this.deck);
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
